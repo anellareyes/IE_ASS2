9f41e2f12849f499574b36cfd53c08ae29abf89e
Eya Reyes <42066915+anellareyes@users.noreply.github.com>
2018-08-05 22:09:50 +0800

    Add files via upload

---
+++ Reyes, Anella (BSIE 1-5N).cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one, two, three, four;
+
+    cout << "Yo! Welcome!"<< endl;
+
+    cout << "This Program is designed to help you bake COOKIES."<<endl;
+
+    cout << "Before baking, make sure you have the following ingredients:" <<endl;
+
+    cout << "3/4 cup granulated sugar, 3/4 cup packed brown sugar, 1 cup butter or margarine, softened, 1 teaspoon vanilla, 1 egg, 2 1/4 cups Gold Medal™ all-purpose flour, 1 teaspoon baking soda, 1/2 teaspoon salt, 1 cup coarsely chopped nuts, 1 package (12 ounces) semisweet chocolate chips (2 cups)" <<endl;
+
+    cout << "To get started, please enter any number from 1-4 to view the steps"<<endl;
+
+    cin>>one;
+
+    cout<<"Step 1. Pre-Heat oven to 375ºF."<<endl;
+    cin>>two;
+
+
+    cout<<"Step 2. Mix sugars, butter, vanilla and egg in large bowl. Stir in flour, baking soda and salt (dough will be stiff). Stir in nuts and chocolate chips."<<endl;
+    cin>>three;
+
+    cout<<"Step 3. Drop dough by rounded tablespoonfuls about 2 inches apart onto ungreased cookie sheet."<<endl;
+    cin>>four;
+
+    cout<<"Step 4. Bake 8 to 10 minutes or until light brown (centers will be soft). Cool slightly; remove from cookie sheet. Cool on wire rack." <<endl;
+
+
+
+
+    cout<<"Thank you" <<endl;
+    system("PAUSE");
+
+
+
+    return 0;
+}
